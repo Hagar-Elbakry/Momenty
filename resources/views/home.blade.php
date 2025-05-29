d538e83d8f794385c3c4467a2ed0c7a57fa3b28f
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-05-30 00:56:10 +0300

    design ui for profile page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="default-avatar" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Hagar Elbakry</h1>
+            </div>
+            <div class="d-flex">
+                <div style="padding-right: 48px">
+                    <strong>153</strong> Posts
+                </div>
+                <div style="padding-right: 48px">
+                    <strong>23K</strong> Followers
+                </div>
+                <div style="padding-right: 48px">
+                    <strong>212</strong> Following
+                </div>
+            </div>
+            <div class="pt-4">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet at dignissimos dolorem dolorum id laboriosam molestiae perspiciatis quasi recusandae, repellat, repellendus rerum sapiente sed similique tenetur, veritatis voluptatem voluptatibus?
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1497445462247-4330a224fdb1?w=500&h=500&fit=crop" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1515023115689-589c33041d3c?w=500&h=500&fit=crop"  alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://images.unsplash.com/photo-1502630859934-b3b41d18206c?w=500&h=500&fit=crop"  alt="" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
