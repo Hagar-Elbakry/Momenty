f573c9409d3b16b63acff0e56046ef0a3bcac263
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-05-31 19:43:23 +0300

    add default profile image and update views to use profileImage method

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = [];
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function profileImage() {
+        return $this->image ? '/storage/' . $this->image : '/images/default-image.jpg';
     }
 }
